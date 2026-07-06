3c5db4ec676584aed1275dc2f1a020a92eaa2616
mssvraju-creator <mssvraju@gmail.com>
2026-07-06 15:26:05 +0000

    refactor: remove serverless-http dependency and related files

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import serverlessHttp from "serverless-http";
-import app from "../artifacts/api-server/src/app";
-
-const serverlessHandler = serverlessHttp(app);
-
-export default function handler(req: any, res: any) {
-  return serverlessHandler(req, res);
-}
